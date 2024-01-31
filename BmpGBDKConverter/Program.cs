75151082e1dbd9be4949e2b0e695d0851e7f3f69
tylerharrold <tylerharrold@gmail.com>
2024-01-30 20:29:11 -0500

    Renamed form in a manner more appropriate

--- BmpGBDKConverter/Program.cs
+++ BmpGBDKConverter/Program.cs
@@ -11,7 +11,7 @@ namespace BmpGBDKConverter
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frm1());
+            Application.Run(new ImportBMPForm());
         }
     }
 }
